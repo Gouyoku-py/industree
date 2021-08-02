fdfdabb1e3e1504e3efa4fa13d6becefe9e15495
John <35625007+Gouyoku-py@users.noreply.github.com>
2021-08-02 14:58:58 +0300

    Initial Streamlit Sharing Setup
    Added all the files necessary to start building the app and deploy it to Streamlit Sharing, i.e.
    - streamlit_app.py: main app file
    - .streamlit/config.toml: configuration file in which page theme, colors and font are specified (for now)
    
    For the time being, the app includes only a page title and a short description for the first tool, "Αλλαγή Κράματος" (Alloy Change).

---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import streamlit as st
+
+st.set_page_config(page_title = 'industree',
+                   page_icon = ':palm_tree:',
+                   layout = 'centered',
+                   initial_sidebar_state = 'auto')
+
+st.title('Αλλαγή Κράματος') # 'Alloy Change'
+
+st.markdown('Μια **απλή** εφαρμογή που δίνει πρόσβαση σε πληροφορίες απαραίτητες \
+            για την αλλαγή κράματος.')
