6c4683c0e86375b55fa60b93dc35a0addae7e60f
John <35625007+Gouyoku-py@users.noreply.github.com>
2021-08-03 16:09:13 +0300

    Add password protection
    Added password protection to provide access only to the people who work at the Casthouse.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,12 @@ st.title('Αλλαγή Κράματος') # 'Alloy Change'
 
 st.markdown('Μια **απλή** εφαρμογή που δίνει πρόσβαση σε πληροφορίες απαραίτητες \
             για την αλλαγή κράματος.')
+
+password = st.text_input('Κωδικός πρόσβασης',
+                         max_chars = 12,
+                         key = 'password',
+                         type = 'password')
+
+if not password == st.secrets['password']: st.stop()
+
+st.write('Ο κωδικός είναι σωστός!')
