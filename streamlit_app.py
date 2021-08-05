57793b4d66bc7ef3b306f3960deb934d33e54ab2
John <35625007+Gouyoku-py@users.noreply.github.com>
2021-08-05 09:32:39 +0300

    Read/write alloy data
    Added the capability to read alloy specs data from csv files stored in Dropbox and write ALL data to the app, using the Dropbox API.
    
    Added environment.yml to include new dependencies, i.e. dropbox and pandas packages, as well as future ones.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,10 @@ This is a temporary script file.
 """
 import streamlit as st
 
+import io
+import dropbox
+import pandas as pd
+
 st.set_page_config(page_title = 'industree',
                    page_icon = ':palm_tree:',
                    layout = 'centered',
@@ -23,4 +27,16 @@ password = st.text_input('Κωδικός πρόσβασης',
 
 if not password == st.secrets['password']: st.stop()
 
-st.write('Ο κωδικός είναι σωστός!')
+dbx = dropbox.Dropbox(st.secrets['dropbox_token'])
+res = dbx.files_download("/specs.csv")[1]
+
+@st.cache()
+def read_data(res):
+    return pd.read_csv(io.BytesIO(res.content),
+                       sep = ';',
+                       header = 0,
+                       index_col = 'ID')
+
+data = read_data(res)
+
+st.dataframe(data)
